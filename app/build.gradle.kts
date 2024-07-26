89ac55d63d4033fdc648a1e27f66f63c445640c4
Richard Walker <Dopameme@outlook.com>
2024-07-26 14:58:21 -0700

    Setup datastore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,8 @@ dependencies {
     implementation(libs.androidx.compose.material3.adaptive.layout)
     implementation(libs.androidx.compose.material3.adaptive.navigation)
     implementation(libs.androidx.compose.material3.windowSizeClass)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.datastore)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
